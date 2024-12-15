4ff20a801cab921b162377a1bd4e6c6f478dbff5
aleksandr1359 <devyatiyarov.alexandr@gmail.com>
2024-12-15 15:56:06 +0500

    feat: resolve 27 exercises

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -236,8 +236,8 @@ function startsWith(str, substr) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
@@ -253,8 +253,13 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const str = '';
+  return str.concat(
+    minutes > 9 ? minutes : minutes.toString().padStart(2, '0'),
+    ':',
+    seconds > 9 ? seconds : seconds.toString().padStart(2, '0')
+  );
 }
 
 /**
@@ -267,8 +272,8 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 /**
